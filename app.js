b5b473a2e1e4239dd4bd28dfaf5be3e841d1f6bf
Oxunjon <oxunjonxatamov32@gmail.com>
2023-05-21 14:19:52 +0500

    teacher add groups json

---
+++ app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+const server = http.createServer(async(req, res) => {
+    if (req.url === "/groups" && req.method === "GET") {
+        const data = JSON.parse(await fs.readFile("groups.json", "utf8"));
+        const data1 = JSON.parse(await fs.readFile("teacher.json", "utf8"));
+        for (let i = 0; i < data.length; i++) {
+            if(data[i].teacher_id === 1) {
+                const data2 = data;
+                data2[0].teacher_id = data1[0];
+                data2[1].teacher_id = data1[1];
+
+                res.end(JSON.stringify(data2));
+            }
+            if(data[i].teacher_id === 2) {
+                const data2 = data;
+                data2[0].teacher_id = data1[1];
+                data2[1].teacher_id = data1[0];
+                
+                res.end(JSON.stringify(data2));
+            };
+        };
+    };
+});
+
+server.listen(8000, () => {
+    console.log(8000);
+});
+
